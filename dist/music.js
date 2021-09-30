4010e6ed003f905d05532b426b875d66711016ab
lion.chuang <lion.chuang@shoalter.com>
2021-09-30 11:55:24 +0800

    Site updated: 2021-09-30 11:55:20

--- dist/music.js
+++ dist/music.js
@@ -9,10 +9,16 @@ const ap = new APlayer({
     listMaxHeight: 60,
     audio: [
         {
-            name: 'music',
+            name: '最近在聽的歌',
             artist: '星茶会',
-            url: 'https://lc-gluttony.s3.amazonaws.com/EbnOjkCxn25E/7RIBkxSDDha5vOfJt3MMLAqnpwbQqdCN/%E6%98%9F%E8%8C%B6%E4%BC%9A%E3%83%94%E3%82%A2%E3%83%8E60%E5%88%86%E8%80%90%E4%B9%85%E4%BD%9C%E6%A5%AD%E7%94%A8BGM%E3%80%90Pianist%20Chinatsuko%E3%80%91.mp3',
-            cover: 'https://i.imgur.com/2RnpRBL.png',
+            url: '/music/星茶会.mp3',
+            cover: '/img/avatar.png',
+        },
+		{
+            name: '最近在聽的歌',
+            artist: 'Fullmetal Alchemist Brotherhood',
+            url: '/music/Fullmetal Alchemist Brotherhood.mp3',
+            cover: '/img/avatar.png',
         }
     ]
 });
